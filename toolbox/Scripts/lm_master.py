1b857cf87c418a3f6f18d101f9851ad7894c061d
Darren <dkav@users.noreply.github.com>
2011-03-23 18:17:34 -0700

    Following coding practice all lines are less than 80 characters in length.

--- toolbox/Scripts/lm_master.py
+++ toolbox/Scripts/lm_master.py
@@ -19,9 +19,9 @@ from time import localtime, strftime # remove DMK
 from string import split # remove DMK
 
 import arcgisscripting
-from numpy import * 
+from numpy import *
 
-import lm_config    
+import lm_config
 import lm_util as lu
 import s1_getAdjacencies as s1
 import s2_buildNetwork as s2
@@ -45,31 +45,31 @@ GP = lm_config.GP
 def lm_master():
     """Main function for linkage mapper
 
-    Called by ArcMap with parameters or run from command line with parameters 
-    entered in script below.  Calls functions in dedicated scripts for each of 
+    Called by ArcMap with parameters or run from command line with parameters
+    entered in script below.  Calls functions in dedicated scripts for each of
     5 processing steps.
-    
+
     """
-    try:    
+    try:
         lu.check_project_dir()
-        
+
         if set("# ").intersection(PROJECTDIR):
-            GP.AddError("\nSpaces are not allowed in project directory path."  
+            GP.AddError("\nSpaces are not allowed in project directory path."
                         "Please avoid using any special characters in "
                         "directory names.")
-            sys.exit(1) # end script process        
+            sys.exit(1) # end script process
 
         def createfolder(lmfolder):
             if not path.exists(lmfolder):
-                GP.CreateFolder_management(path.dirname(lmfolder), 
-                                       path.basename(lmfolder))                         
+                GP.CreateFolder_management(path.dirname(lmfolder),
+                                       path.basename(lmfolder))
         createfolder(OUTPUTDIR)
         createfolder(LOGDIR)
         createfolder(SCRATCHDIR)
         createfolder(DATAPASSDIR)
-            
+
         # Check to make sure no missing steps, and identify first step checked
-        lu.check_steps() 
+        lu.check_steps()
         if STEP5: firstStep = 5
         if STEP4:
             firstStep = 4
@@ -90,30 +90,31 @@ def lm_master():
             lu.dashline(1)
             msg = 'ERROR: Please check at least one step.'
             GP.AddError(msg)
-            exit(1)       
+            exit(1)
 
-        # Make a backup copy of datapass directory.  
+        # Make a backup copy of datapass directory.
         # This will have lcp maps and link tables from previous run.
-        lu.archive_datapass()   
+        lu.archive_datapass()
 
-        lu.clean_up_link_tables(firstStep)    
-        
-        if STEP5:               
+        lu.clean_up_link_tables(firstStep)
+
+        if STEP5:
             if GP.Exists(OUTPUTGDB):
                 GP.addmessage('Deleting geodatabase ' + OUTPUTGDB)
                 try:
                     GP.delete_management(OUTPUTGDB)
                 except:
                     lu.dashline(1)
-                    msg = 'ERROR: Could not remove geodatabase ' + OUTPUTGDB + '. Is it open in ArcMap?'
+                    msg = ('ERROR: Could not remove geodatabase ' + OUTPUTGDB +
+                          '. Is it open in ArcMap?')
                     GP.AddError(msg)
                     exit(1)
-            #move_stick_maps(firstStep)        
-                                
-        # Step 1    
+            #move_stick_maps(firstStep)
+
+        # Step 1
         if STEP1:
             lu.dashline(1)
-            GP.addmessage('Running script s1_getAdjacencies.py')            
+            GP.addmessage('Running script s1_getAdjacencies.py')
             s1.step1_get_adjacencies()
 
         # Step 2
@@ -124,23 +125,23 @@ def lm_master():
 
         # Step 3
         if STEP3:
-            lu.dashline(1)       
+            lu.dashline(1)
             GP.addmessage('Running script s3_calcCwds.py')
             s3.step3_calc_cwds()  # Run step 3
 
-        # Step 4    
+        # Step 4
         if STEP4 :
-            lu.dashline(1)        
+            lu.dashline(1)
             GP.addmessage('Running script s4_refineNetwork.py')
-            s4.step4_refine_network()  # Run step 4            
-        
-        # Step 5    
+            s4.step4_refine_network()  # Run step 4
+
+        # Step 5
         if STEP5 :
-            lu.dashline(1)        
+            lu.dashline(1)
             GP.addmessage('Running script s5_calcLccs.py')
-            s5.step5_calc_lccs() # Run step 5            
-                
-        GP.addmessage('\nDONE!\n')                
+            s5.step5_calc_lccs() # Run step 5
+
+        GP.addmessage('\nDONE!\n')
 
     # Return GEOPROCESSING specific errors
     except arcgisscripting.ExecuteError:
@@ -151,6 +152,6 @@ def lm_master():
     except:
         filename =  'lm_master.py'
         lu.raise_python_error(filename)
-        
+
 if __name__ == "__main__":
     lm_master()
