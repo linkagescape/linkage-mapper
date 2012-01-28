e0ed230b6f8a8550f99343f918308e8ec2262e8c
Brad McRae <mcrae@circuitscape.org>
2012-01-28 12:30:02 -0800

    Changes to build file.

--- resources/lm_build.bat
+++ resources/lm_build.bat
@@ -2,4 +2,4 @@ xcopy /E/I/Y ..\demo ..\build\LM_demo
 xcopy /Y ..\toolbox\doc\*.pdf ..\build\*.pdf
 xcopy /E/I/Y ..\toolbox\scripts ..\build\toolbox\scripts
 xcopy /E/I/Y ..\toolbox\styles ..\build\toolbox\styles
-xcopy /Y ..\toolbox\*Mapper.tbx ..\build\toolbox\*Mapper.tbx
+xcopy /Y ..\toolbox\*.tbx ..\build\toolbox\*.tbx
