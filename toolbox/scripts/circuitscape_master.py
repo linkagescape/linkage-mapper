a8c98f7702560b9988498b936db2ff9784da8138
Darren <dkav@users.noreply.github.com>
2012-02-08 10:46:39 -0800

    Major update to configuration module. Minor updates and corrections throughout.

--- toolbox/scripts/circuitscape_master.py
+++ toolbox/scripts/circuitscape_master.py
@@ -9,111 +9,108 @@ Numpy
 
 """
 
-
 import os.path as path
-import arcgisscripting
-from lm_config import Config as Cfg
-import lm_util as lu
 import shutil
+import sys
 
-import os
+import arcgisscripting
 
-import s8_pinchpoints as s8 
+from lm_config import tool_env as cfg
+import lm_util as lu
 import s7_centrality as s7
+import s8_pinchpoints as s8
+
 
 _filename = path.basename(__file__)
 
-gp = Cfg.gp
-if not Cfg.LOGMESSAGES:
-    gprint = gp.addmessage
-else:
-    gprint = lu.gprint
 
 def circuitscape_master():
     """
-    
+
     """
+    cfg.configure("circuitscape", sys.argv)
+    gp = cfg.gp
+    gprint = lu.gprint
     try:
-        lu.createfolder(Cfg.LOGDIR)
-        lu.createfolder(Cfg.MESSAGEDIR)
-        Cfg.logFile=lu.create_log_file(Cfg.MESSAGEDIR, Cfg.TOOL, Cfg.PARAMS)
-        
+        lu.createfolder(cfg.LOGDIR)
+        lu.createfolder(cfg.MESSAGEDIR)
+        cfg.logFile = lu.create_log_file(cfg.MESSAGEDIR, cfg.TOOL, cfg.PARAMS)
+
         # Check core ID field.
-        lu.check_cores(Cfg.COREFC, Cfg.COREFN) 
-        
-        gp.OutputCoordinateSystem = gp.describe(Cfg.COREFC).SpatialReference
+        lu.check_cores(cfg.COREFC, cfg.COREFN)
+
+        gp.OutputCoordinateSystem = gp.describe(cfg.COREFC).SpatialReference
         gp.pyramid = "NONE"
-        gp.rasterstatistics = "NONE"              
-                                               
+        gp.rasterstatistics = "NONE"
+
         # Move adj and cwd results from earlier versions to datapass directory
         lu.move_old_results()
-              
-        lu.delete_dir(Cfg.SCRATCHDIR)
-                     
-        if Cfg.DOPINCH == False and Cfg.DOCENTRALITY == False:            
+
+        lu.delete_dir(cfg.SCRATCHDIR)
+
+        if not cfg.DOPINCH and not cfg.DOCENTRALITY:
             msg = ('ERROR: Please choose at least one option: pinch point or\n'
                     'network centrality analysis.')
             lu.raise_error(msg)
 
-        lu.createfolder(Cfg.SCRATCHDIR) 
-        lu.createfolder(Cfg.ARCSCRATCHDIR)
-        
-        if Cfg.DO_ALLPAIRS == True:
+        lu.createfolder(cfg.SCRATCHDIR)
+        lu.createfolder(cfg.ARCSCRATCHDIR)
+
+        if cfg.DO_ALLPAIRS:
             #  Fixme: move raster path to config
-            S5CORRIDORRAS = os.path.join(Cfg.OUTPUTGDB,Cfg.PREFIX + 
-                                         "_lcc_mosaic_int") 
+            S5CORRIDORRAS = path.join(cfg.OUTPUTGDB, cfg.PREFIX +
+                                         "_lcc_mosaic_int")
             if not gp.Exists(S5CORRIDORRAS):
                 msg = ('ERROR: Corridor raster created in step 5 is required'
                         '\nfor all-pair analyses, but was not found.')
                 lu.raise_error(msg)
-        if Cfg.DOPINCH == True:
+        if cfg.DOPINCH:
             # Make a local grid copy of resistance raster-
             # will run faster than gdb.
-            lu.delete_data(Cfg.RESRAST)
-            if not gp.Exists(Cfg.RESRAST_IN):
+            lu.delete_data(cfg.RESRAST)
+            if not gp.Exists(cfg.RESRAST_IN):
                 msg = ('ERROR: Resistance raster is required for pinch point'
                         ' analyses, but was not found.')
                 lu.raise_error(msg)
-                  
-            arcpy.env.extent = Cfg.RESRAST_IN
-            arcpy.env.snapRaster = Cfg.RESRAST_IN
-            gprint('\nMaking local copy of resistance raster.')
-            gp.CopyRaster_management(Cfg.RESRAST_IN, Cfg.RESRAST)          
-                    
-        
-        if Cfg.DOCENTRALITY == True:             
-            gprint("Creating output folder: " + Cfg.CENTRALITYBASEDIR)
-            if path.exists(Cfg.CENTRALITYBASEDIR):
-                shutil.rmtree(Cfg.CENTRALITYBASEDIR)
-            lu.createfolder(Cfg.CENTRALITYBASEDIR)
-            gp.CreateFolder_management(Cfg.CENTRALITYBASEDIR, 
-                                        Cfg.CIRCUITOUTPUTDIR_NM)
-            gp.CreateFolder_management(Cfg.CENTRALITYBASEDIR, 
-                                        Cfg.CIRCUITCONFIGDIR_NM)    
-            lu.clean_out_workspace(Cfg.CORECENTRALITYGDB)
-            
-            s7.STEP7_calc_centrality()
-            if Cfg.SAVECENTRALITYDIR == False:
-                lu.delete_dir(Cfg.CENTRALITYBASEDIR)
-    
-        if Cfg.DOPINCH == True:     
-            gprint("Creating output folder: " + Cfg.CIRCUITBASEDIR)
-            lu.delete_dir(Cfg.CIRCUITBASEDIR)
-            lu.createfolder(Cfg.CIRCUITBASEDIR)
-            gp.CreateFolder_management(Cfg.CIRCUITBASEDIR, 
-                                        Cfg.CIRCUITOUTPUTDIR_NM)
-            gp.CreateFolder_management(Cfg.CIRCUITBASEDIR, 
-                                        Cfg.CIRCUITCONFIGDIR_NM)                 
-            
-            lu.clean_out_workspace(Cfg.PINCHGDB)
-            lu.delete_data(Cfg.PINCHGDB)
-            
-            s8.STEP8_calc_pinchpoints()            
 
-            lu.delete_dir(Cfg.SCRATCHDIR)
-            if Cfg.SAVECIRCUITDIR == False:
-                lu.delete_dir(Cfg.CIRCUITBASEDIR)
-            
+            arcpy.env.extent = cfg.RESRAST_IN
+            arcpy.env.snapRaster = cfg.RESRAST_IN
+            gprint('\nMaking local copy of resistance raster.')
+            gp.CopyRaster_management(cfg.RESRAST_IN, cfg.RESRAST)
+
+        if cfg.DOCENTRALITY:
+            gprint("Creating output folder: " + cfg.CENTRALITYBASEDIR)
+            if path.exists(cfg.CENTRALITYBASEDIR):
+                shutil.rmtree(cfg.CENTRALITYBASEDIR)
+            lu.createfolder(cfg.CENTRALITYBASEDIR)
+            gp.CreateFolder_management(cfg.CENTRALITYBASEDIR,
+                                        cfg.CIRCUITOUTPUTDIR_NM)
+            gp.CreateFolder_management(cfg.CENTRALITYBASEDIR,
+                                        cfg.CIRCUITCONFIGDIR_NM)
+            lu.clean_out_workspace(cfg.CORECENTRALITYGDB)
+
+            s7.STEP7_calc_centrality()
+            if not cfg.SAVECENTRALITYDIR:
+                lu.delete_dir(cfg.CENTRALITYBASEDIR)
+
+        if cfg.DOPINCH:
+            gprint("Creating output folder: " + cfg.CIRCUITBASEDIR)
+            lu.delete_dir(cfg.CIRCUITBASEDIR)
+            lu.createfolder(cfg.CIRCUITBASEDIR)
+            gp.CreateFolder_management(cfg.CIRCUITBASEDIR,
+                                        cfg.CIRCUITOUTPUTDIR_NM)
+            gp.CreateFolder_management(cfg.CIRCUITBASEDIR,
+                                        cfg.CIRCUITCONFIGDIR_NM)
+
+            lu.clean_out_workspace(cfg.PINCHGDB)
+            lu.delete_data(cfg.PINCHGDB)
+
+            s8.STEP8_calc_pinchpoints()
+
+            lu.delete_dir(cfg.SCRATCHDIR)
+            if not cfg.SAVECIRCUITDIR:
+                lu.delete_dir(cfg.CIRCUITBASEDIR)
+
         gprint('\nDONE!\n')
 
     # Return GEOPROCESSING specific errors
@@ -123,7 +120,7 @@ def circuitscape_master():
     # Return any PYTHON or system specific errors
     except:
         lu.print_python_error(_filename)
-       
-        
+
+
 if __name__ == "__main__":
     circuitscape_master()
