a5fcd266d9cb0440eadb93a7b78267653b98b191
Darren Kavanagh <dkav@users.noreply.github.com>
2012-04-10 14:04:35 -0700

    Minor fix to climate corridor code.

---
+++ toolbox/scripts/lm_settings.py
@@ -0,0 +1,8 @@
+### USER SETTABLE VARIABLES
+CALCNONNORMLCCS = False  # Mosiac non-normalized LCCs (Boolean)
+WRITETRUNCRASTER = True  # Write truncated mosaicked raster (Boolean)
+CWDTHRESH = 200000  # CWD corridor width in a truncated raster (Integer)
+MINCOSTDIST = None  # Minimum cost distance (Integer)
+MINEUCDIST = None  # Minimum euclidean distance (Integer)
+SAVENORMLCCS = False  # Save inidvidual normalized LCC grids (Boolean)
+SIMPLIFY_CORES = True  # Simplify cores before calcuting distances (Boolean)
