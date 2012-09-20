07b846edaac66a2f7ac197335ea9113032458e0a
Darren Kavanagh <dkav@users.noreply.github.com>
2012-09-19 17:45:17 -0700

    Added comments to script that runs Climate Tool.

--- resources/cc_run.py
+++ resources/cc_run.py
@@ -11,21 +11,31 @@ _SCRIPT_NAME = path.basename(__file__)
 
 def main():
     """Runs Climate Corridor tool"""
-    proj_dir = "W:\\ProjectAscEq"
-    core_fc = "W:\\Base Test\\PROJECT\\in_data\\cores.shp"
-    core_fl = "HCA_ID"
-    climate_rast = "W:\\Base Test\\PROJECT\\in_data\\climate.img"
-    #resis_rast = "#"
+    proj_dir = "W:\\ProjectAscEq"  # Project Directory
+    
+	# Core Inputs
+	core_fc = "W:\\Base Test\\PROJECT\\in_data\\cores.shp"  # Core Feature Class
+    core_fl = "HCA_ID"  # Core Identifer in Core Feature Class
+    
+	climate_rast = "W:\\Base Test\\PROJECT\\in_data\\climate.img"  # Climate Raster
+    
+	# Resistance Raster
+	#resis_rast = "#"  # Can be run without resistance raster
     resis_rast = "W:\\Base Test\\PROJECT\\in_data\\resist.img"
+	
+	# GRASS GIS Folder
     #gisbase = "C:\\Program Files (x86)\\GRASS 6.4.2"
     #gisbase = "C:\\Program Files (x86)\\GRASS GIS 6.5.svn"
     gisbase = "C:\Program Files (x86)\GRASS GIS 7.0.svn"
-    min_euc_dist = 2000
-    max_euc_dist = 50000
-    climate_threashold = 1
-    climate_cost = 50000
+	
+    # Model Settings
+	min_euc_dist = 2000  # Minimum Euclidean Distance
+    max_euc_dist = 50000  # Maximum Euclidean Distance
+    climate_threashold = 1  # Climate Threashold (e.g. degrees Celsius)
+    climate_cost = 50000  #  Climate Cost (e.g. 1 degree C change equals 50,000 m distance cost)
 
-    sys.path.append('..\\toolbox\\scripts')
+    # Setup path and run model
+	sys.path.append('..\\toolbox\\scripts')
     import cc_main
 
     argv = (_SCRIPT_NAME, proj_dir, core_fc, core_fl, climate_rast, resis_rast,
