5e4b0b16d9a33a83ca16dc764a0feedf42a0b066
Darren Kavanagh <Darren Kavanagh@localhost>
2012-10-23 02:01:20 -0700

    Fix to allow climate tool work in ArcGIS. Many other improvments too.

---
+++ resources/lm_build.bat
@@ -0,0 +1,81 @@
+@ECHO OFF
+IF "%1"=="" GOTO Stop
+
+echo Building Linkage Mapper Release number %1
+
+@ECHO ON
+xcopy /E/I/Y ..\demo ..\LinkageMapper"%1"\LM_demo
+xcopy /Y ..\toolbox\doc\*.pdf ..\LinkageMapper"%1"\*.pdf
+xcopy /E/I/Y ..\toolbox\scripts ..\LinkageMapper"%1"\toolbox\scripts
+xcopy /E/I/Y ..\toolbox\styles ..\LinkageMapper"%1"\toolbox\styles
+xcopy /Y ..\toolbox\*.tbx ..\LinkageMapper"%1"\toolbox\*.tbx
+
+del ..\LinkageMapper"%1"\toolbox\scripts\cc*.*
+del ..\LinkageMapper"%1"\toolbox\scripts\s7*.*
+del ..\LinkageMapper"%1"\toolbox\scripts\s6*.*
+del ..\LinkageMapper"%1"\toolbox\scripts\bar*.*
+del ..\LinkageMapper"%1"\toolbox\climate*.*
+del ..\LinkageMapper"%1"\LM_demo\cc*.*
+del ..\LinkageMapper"%1"\LM_demo\demoData\cc*.*
+del ..\LinkageMapper"%1"\toolbox\*Plus.tbx
+del ..\LinkageMapper"%1"\toolbox\*Extras.tbx
+
+@ECHO OFF
+if "%2"=="cc" GOTO CCCopy
+if "%3"=="cc" GOTO CCCopy
+if "%4"=="cc" GOTO CCCopy
+GOTO CMTest
+
+:CCCopy
+@ECHO ON
+xcopy /Y ..\toolbox\scripts\cc*.* ..\LinkageMapper"%1"\toolbox\scripts\cc*.*
+xcopy /Y ..\toolbox\climate*.tbx ..\LinkageMapper"%1"\toolbox\climate*.tbx
+xcopy /Y ..\demo\cc*.* ..\LinkageMapper"%1"\LM_demo\cc*.*
+xcopy /Y ..\demo\demoData\cc*.* ..\LinkageMapper"%1"\LM_demo\demoData\cc*.*
+
+:CMTest
+@ECHO OFF
+if "%2"=="cm" GOTO CMCopy
+if "%3"=="cm" GOTO CMCopy
+if "%4"=="cm" GOTO CMCopy
+GOTO BMTest
+
+:CMCopy
+@ECHO ON
+xcopy /Y ..\toolbox\scripts\s7_c*.* ..\LinkageMapper"%1"\toolbox\scripts\s7_c*.*
+REM xcopy /Y ..\toolbox\*Extras.tbx ..\LinkageMapper"%1"\toolbox\*Extras.tbx
+REM xcopy /Y ..\toolbox\doc\cen*.doc ..\LinkageMapper"%1"\cen*.doc
+
+:BMTest
+@ECHO OFF
+if "%2"=="bm" GOTO BMCopy
+if "%3"=="bm" GOTO BMCopy
+if "%4"=="bm" GOTO BMCopy
+GOTO Finish
+
+:BMCopy
+@ECHO ON
+xcopy /Y ..\toolbox\scripts\bar*.* ..\LinkageMapper"%1"\toolbox\scripts\bar*.*
+xcopy /Y ..\toolbox\scripts\s6_b*.* ..\LinkageMapper"%1"\toolbox\scripts\s6_b*.*
+REM xcopy /Y ..\toolbox\*Extras.tbx ..\LinkageMapper"%1"\toolbox\*Extras.tbx
+REM xcopy /Y ..\toolbox\doc\barr*.docx ..\LinkageMapper"%1"\barr*.docx
+REM xcopy /Y ..\toolbox\doc\McRae*.pdf ..\LinkageMapper"%1"\McRae*.pdf
+
+:Finish
+@ECHO OFF
+echo releaseNum = "%1" > ..\LinkageMapper"%1"\toolbox\scripts\lm_version.py
+echo.
+echo Finished building Linkage Mapper release number %1
+if "%2"=="" GOTO END
+echo Options: %2 %3 %4
+GOTO END
+
+:Stop
+@ECHO OFF
+echo.
+echo Error: must supply a version string
+echo syntax: lm_build (version) cm bm cc
+echo example: lm_build 0.8.4 cm cc
+echo.
+
+:End
---
+++ toolbox/scripts/cc_config.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python2.6
+# Author: Darren Kavanagh
+
+"""Climate Corridor configuration module.
+
+Assigns input parameters from ToolBox to variables, and sets constants
+
+"""
+
+import os
+import sys
+
+
+def nullstring(arg_string):
+    """Convert ESRI nullstring to Python null"""
+    if arg_string == "#":
+        arg_string = None
+    return arg_string
+
+
+class ClimateConfig():
+    """Class container to hold Climate Tool global variables"""
+    def __init__(self):
+        """Init class (empty)"""
+        pass
+
+    def configure(self, arg):
+        """Setup input parameters"""
+        # Input files
+        self.proj_dir = arg[1]  # Project directory
+        self.core_fc = arg[2]  # Core area feature class
+        self.core_fld = arg[3]  # Core area field name
+        self.climate_rast = arg[4]  # Climate raster (+ path)
+        self.resist_rast = nullstring(arg[5])  # Resistance raster (+ path)
+
+        # Setup GRASS base folder environmental setting
+        self.gisbase = os.environ['GISBASE'] = arg[6]  # GRASS path
+        sys.path.append(os.path.join(os.environ['GISBASE'], "etc", "python"))
+
+        # Tool settings
+        self.min_euc_dist = float(arg[7])  # Min distance between core pairs
+        self.max_euc_dist = float(arg[8])  # Max distance between core pairs
+        self.climate_threashold = float(arg[9])  # Temperature threashold
+        self.climate_cost = float(arg[10])  # Cost incurred by climate variable
+
+        # Prune network settings
+        # For Linkage Mapper. No type conversions necessary
+        self.prune_network = arg[11]
+        self.max_nn = arg[12]  # No of connected nearest neighbors
+        self.nn_unit = arg[13]  # NN Unit
+        self.keep_constelations = arg[14]
+
+        # Setup model global variables
+        self.code_dir = os.path.dirname(os.path.abspath(__file__))
+        self.out_dir = os.path.join(self.proj_dir, "clm_corr")  # CC directory
+        self.prj_area_rast = os.path.join(self.out_dir, "projarea")
+        self.prj_core_fc = (os.path.join(
+                            self.out_dir, "cores.shp"))  # Proj core area  name
+        self.prj_climate_rast = os.path.join(self.out_dir, "climate")
+        self.simplify_cores = True
+        self.core_simp = os.path.join(cc_env.out_dir, "coresim.shp")
+
+        # Use project area raster if no resistance layer is provided
+        if self.resist_rast is not None:
+            self.prj_resist_rast = os.path.join(self.out_dir, "resist")
+        else:
+            self.prj_resist_rast = self.prj_area_rast
+
+
+cc_env = ClimateConfig()
