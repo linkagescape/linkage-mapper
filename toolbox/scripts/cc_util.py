7c8b05ad5d323830bf911ef2b453da3ec0f9e3ca
Darren Kavanagh <dkav@uw.edu>
2014-11-11 12:47:50 -0800

    Code cleanup and fixes to Climate Linkage Mapper modules. Updates to support GRASS 7. Changes include: - Updated rc_rules variable to match GRASS 7 back raster output - GRASS path update - No longer supporting GRASS 6.x . GRASS version no longer returned by   cc_grass_cwd.setup_wkrspace - Updated Climate Linkage Mapper User Guide to reflect version 7 support - Simplified cc_util.remove_grass_wkspc function - Deleted gdal_check and integrated with gdal_failed_check. Moved to cc_util.   Added check for empty string - Modified cc_util.delete_feature function to allow the deletion of multiple   features at once. Renamed to delete_features - Modularlized code in cc_main.main - Created sub-function, create_arcgrid, to output ARCINFO grids from GRASS - Removed commented out code - Deleted result history from CC Demo_Arc10 map document

--- toolbox/scripts/cc_util.py
+++ toolbox/scripts/cc_util.py
@@ -4,10 +4,13 @@
 """Climate Linkage Mapper utility module"""
 
 import os
+import subprocess
+import shutil
 
 import arcpy
 
 from cc_config import cc_env
+import lm_util
 
 
 def mk_proj_dir(in_dir):
@@ -18,26 +21,30 @@ def mk_proj_dir(in_dir):
     return new_dir
 
 
-def delete_feature(in_feature):
-    """Delete temporary feature if already exists"""
-    if arcpy.Exists(in_feature):
-        try:
-            arcpy.Delete_management(in_feature)
-        except arcpy.ExecuteError:
-            arcpy.AddWarning("Error deleting temporary %s. Program will "
-                             "continue." % in_feature)
+def delete_features(in_features):
+    """Delete temporary feature/s if it exists"""
+    for in_feature in in_features:
+        if arcpy.Exists(in_feature):
+            try:
+                arcpy.Delete_management(in_feature)
+            except arcpy.ExecuteError:
+                arcpy.AddWarning("Error deleting temporary %s. Program will "
+                                 "continue." % in_feature)
+
 
 def add_grass_path(gisbase):
+    """Add GRASS directories to system PATH"""
     env_list = os.environ['PATH'].split(';')
     env_list.insert(0, os.path.join(gisbase, "msys", "bin"))
-    env_list.insert(0, os.path.join(gisbase, "extralib"))
+    env_list.insert(0, os.path.join(gisbase, "extrabin"))
     env_list.insert(0, os.path.join(gisbase, "bin"))
     env_list.insert(0, os.path.join(gisbase, "lib"))
     env_list.insert(0, os.path.join(gisbase, "etc", "python"))
     env_list.insert(0, os.path.join(gisbase, "etc"))
-    os.environ['PATH'] = ';'.join(env_list)  # Path should now prioritize 
-                                             # proper gdal dll location
-                             
+    # Path should now prioritize proper gdal dll location
+    os.environ['PATH'] = ';'.join(env_list)
+
+
 def check_cc_project_dir():
     """Checks to make sure path name is not too long.
 
@@ -46,31 +53,61 @@ def check_cc_project_dir():
     if len(cc_env.proj_dir) > 140:
         msg = ('ERROR: Project directory "' + cc_env.proj_dir +
                '" is too deep.  Please choose a shallow directory'
-               '(something like "C:\PUMA").')
+               r'(something like "C:\PUMA").')
         raise Exception(msg)
 
-    if "-" in cc_env.proj_dir or " " in cc_env.proj_dir or "." in cc_env.proj_dir:
+    if ("-" in cc_env.proj_dir or " " in cc_env.proj_dir or
+            "." in cc_env.proj_dir):
         msg = ('ERROR: Project directory cannot contain spaces, dashes, or '
-                'special characters.')
+               'special characters.')
         raise Exception(msg)
     return
-    
-    
+
+
 def remove_grass_wkspc(gisdbase):
-        # Remove GRASS workspace from earlier run if any
-        if os.path.exists(gisdbase):
-            import shutil
-            try:
-                shutil.rmtree(gisdbase, True)
-            except:
-                arcpy.AddWarning("\nCannot delete GRASS workspace from earlier"
-                                " run."
-                                "\nPlease choose a new project directory.")
-                raise Exception("Cannot delete GRASS workspace: " + gisdbase)              
-            if os.path.exists(gisdbase):
-                arcpy.AddWarning("\nCannot delete GRASS workspace from earlier"
-                                " run."
-                                "\nPlease choose a new project directory.")
-                raise Exception("Cannot delete GRASS workspace: " + gisdbase)  
-    
-                             
+    """Remove GRASS workspace if it exists"""
+    if os.path.exists(gisdbase):
+        try:
+            shutil.rmtree(gisdbase, True)
+        except OSError:
+            return False
+    return True
+
+
+def gdal_fail_check():
+    """Code to check GDAL dlls and system path"""
+    gdal = subprocess.Popen("where gdal*", stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE, shell=True).stdout.read()
+
+    if gdal != '':
+        gdal_list = gdal.split('\n')
+        if 'arcgis' in gdal_list[1].lower():
+            lm_util.gprint("\nGDAL DLL/s found at: " + gdal)
+            arcpy.AddWarning("It looks like there is a conflict between "
+                             "ArcGIS")
+            arcpy.AddWarning("and GRASS. This could be the result of a "
+                             "previous ")
+            arcpy.AddWarning("analysis (like a Linkage Mapper run) or it might"
+                             "be")
+            arcpy.AddWarning("caused by conflicts with pre-loaded ArcGIS ")
+            arcpy.AddWarning("extensions.")
+            arcpy.AddWarning("\nThis error often goes away if you run the tool"
+                             "in")
+            arcpy.AddWarning("the background (see user guide). ")
+            arcpy.AddWarning("\nIf that doesn't work, try restarting ArcMap.")
+            arcpy.AddWarning("\nIf you still get an error, then restart again")
+            arcpy.AddWarning(" and disable any extensions you are not using")
+            arcpy.AddWarning("(Click on Customize >> Extensions).")
+            arcpy.AddWarning("\nAnd if that doesn't work try closing Arc and ")
+            arcpy.AddWarning("instead run the tool using the "
+                             "'CC Run Script.py' ")
+            arcpy.AddWarning("python script.  This script can be found in "
+                             "the ")
+            arcpy.AddWarning("'demo' directory, located where the Linkage")
+            arcpy.AddWarning("Mapper toolbox is installed.\n")
+            raise Exception("ArcGIS-GRASS GDAL DLL conflict")
+    else:
+        lm_util.gprint("GDAL DLL/s not found in system PATH")
+        arcpy.AddWarning("Check if the appropriate version of GRASS is "
+                         "correctly installed.")
+        raise Exception("GRASS DLL/s not found")
