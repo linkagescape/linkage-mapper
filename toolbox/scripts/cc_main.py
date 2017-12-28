ff233b8fbbbeac8edcbcb2ba7a1b668acc26d35d
Annie Prisbrey <annie.prisbrey@consbio.org>
2017-12-28 14:39:47 -0700

    Fixed parameter type mismatch in climate LM log file
    Changed parentheses to brackets to fix compatibility issue between climate LM and LPM

--- toolbox/scripts/cc_main.py
+++ toolbox/scripts/cc_main.py
@@ -108,10 +108,10 @@ def arc_wksp_setup():
 
 def config_lm():
     """Configure Linkage Mapper"""
-    lm_arg = (_SCRIPT_NAME, cc_env.proj_dir, cc_env.prj_core_fc,
+    lm_arg = [_SCRIPT_NAME, cc_env.proj_dir, cc_env.prj_core_fc,
               cc_env.core_fld, cc_env.prj_resist_rast, "false", "false", "#",
               "#", "true", "false", cc_env.prune_network, cc_env.max_nn,
-              cc_env.nn_unit, cc_env.keep_constelations, "true", "#", "#", "#")
+              cc_env.nn_unit, cc_env.keep_constelations, "true", "#", "#", "#"]
     lm_env.configure(lm_env.TOOL_CC, lm_arg)
     lm_util.create_dir(lm_env.DATAPASSDIR)
     lm_util.gprint('\nClimate Linkage Mapper Version ' + lm_env.releaseNum)
