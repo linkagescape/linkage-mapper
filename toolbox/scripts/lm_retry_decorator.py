f4e49042c81cbb992d36900ad5f9cf993d8024e1
Darren Kavanagh <dkav@users.noreply.github.com>
2018-04-02 20:29:13 -0700

    Clean up lm_retry_decorator
    Clean up code based on output from Flake8 and Pylint.

--- toolbox/scripts/lm_retry_decorator.py
+++ toolbox/scripts/lm_retry_decorator.py
@@ -1,18 +1,24 @@
-# Code written by Peter Hoffmann.  Modified by Brad McRae for Linkage Mapper.
+"""Decorator for retrying a function if exception occurs.
+
+Code written by Peter Hoffmann.  Modified by Brad McRae for Linkage Mapper.
+
+http://peter-hoffmann.com/2010/retry-decorator-python.html
+
+"""
 
 import sys
 import traceback
-import time
-import arcgisscripting
+
 import lm_util as lu
-gp = arcgisscripting.create(9.3)
-gprint = gp.addmessage
+
 
 class Retry(object):
+    """Decorator for retrying a function if exception occurs."""
+
     default_exceptions = (Exception,)
+
     def __init__(self, tries, exceptions=None, delay=1):
-        """
-        Decorator for retrying a function if exception occurs
+        """Init retry decorator.
 
         tries -- num tries
         exceptions -- exceptions to catch
@@ -20,39 +26,42 @@ class Retry(object):
         """
         self.tries = tries
         if exceptions is None:
-            exceptions = retry.default_exceptions
-        self.exceptions =  exceptions
+            exceptions = Retry.default_exceptions
+        self.exceptions = exceptions
         self.delay = delay
 
-    def __call__(self, f):
-        def fn(*args, **kwargs):
+    def __call__(self, func_in):
+        """Run decorator function."""
+        def func_out(*args, **kwargs):
+            """Run and if necessary retry running function."""
             exception = None
-            for _ in range(self.tries):
+            for i in range(self.tries):
                 try:
-                    return f(*args, **kwargs)
-                except self.exceptions, e:
-                    import traceback
-                    tb = sys.exc_info()[2]  # get the traceback object
+                    return func_in(*args, **kwargs)
+                except self.exceptions, func_error:
+                    tbobj = sys.exc_info()[2]  # Get the traceback object
                     # tbinfo contains the error's line number and the code
-                    tbinfo = traceback.format_tb(tb)[1]
+                    tbinfo = traceback.format_tb(tbobj)[1]
                     line = tbinfo.split(", ")[1]
                     filename = tbinfo.split(", ")[0]
                     filename = filename.rsplit("File ")[1]
-                    lu.warn('--------------------------------------------------')
+                    lu.warn('------------------------------------------------'
+                            '--')
                     msg = ("The following error is being reported "
                            "on " + line + " of " + filename + ":")
                     lu.warn(msg)
-                    lu.warn(str(e))
+                    lu.warn(str(func_error))
                     lu.print_drive_warning()
 
-                    delaytime = self.delay*10*(_ + 1)
+                    delaytime = self.delay * 10 * (i + 1)
                     lu.warn("Will try again. ")
-                    lu.warn('---------RETRY #' + str(_+1) + ' OUT OF ' +
-                                  str(self.tries) + ' IN ' +
-                                  str(delaytime) + ' SECONDS---------\n')
+                    lu.warn('---------RETRY #' + str(i + 1) + ' OUT OF ' +
+                            str(self.tries) + ' IN ' +
+                            str(delaytime) + ' SECONDS---------\n')
                     lu.snooze(delaytime)
-                    exception = e
+                    exception = func_error
 
-            #if no success after tries, raise last exception
+            # If no success after tries, raise last exception
             raise exception
-        return fn
+
+        return func_out
