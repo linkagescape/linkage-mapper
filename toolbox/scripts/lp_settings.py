9ae3c472d02f45fe059d03554a62b9d376304a2d
Darren Kavanagh <dkav@users.noreply.github.com>
2019-01-17 16:59:24 -0800

    Add comments to settings file

--- toolbox/scripts/lp_settings.py
+++ toolbox/scripts/lp_settings.py
@@ -4,6 +4,7 @@
 # Calculate Corridor Specific Value (CSP) or CSP & Blended Priority (BP)
 CALCCSPBP = 2  # No_Calc=0, CSP=1, CSP_BP=2
 
+# Normalization methods - "SCORE_RANGE" or "MAX_VALUE"
 RELPERMNORMETH = "SCORE_RANGE"  # relative permeability normalization method
 RELCLOSENORMETH = "SCORE_RANGE"  # relative closeness value normalization method
 RESNORMETH = "MAX_VALUE"  # resistance normalization method
@@ -19,6 +20,6 @@ MINCPV = 0  # minimum corridor priority value (use 0 to keep all)
 MAXCSPWEIGHT = 0.5  # relative max CSP value weight in CPV calculation
 MEANCSPWEIGHT = 0.5  # relative mean CSP value weight in CPV calculation
 
-HIGHERCE_COOLER = False  # higher climate envelop values are cooler
+HIGHERCE_COOLER = False  # higher climate envelop values are cooler (Boolean)
 
-KEEPINTERMEDIATE = True  # keep intermediate outputs for troubleshooting purposes
+KEEPINTERMEDIATE = True  # keep intermediate outputs for troubleshooting purposes (Boolean)
