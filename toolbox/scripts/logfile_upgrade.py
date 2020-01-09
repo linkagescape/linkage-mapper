3346de30036e65aa0ed7dbc0bf6252109bee1db6
Darren Kavanagh <dkav@users.noreply.github.com>
2020-01-09 12:26:35 -0800

    Add tool to update v1 logfiles to v2

---
+++ toolbox/scripts/logfile_upgrade.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python2
+
+"""Upgrade v1.x logfile to v2.0 logfile."""
+
+import os
+import sys
+import glob
+from ast import literal_eval
+
+import arcpy
+
+
+FILE_PREFIX = "_v2_Linkage"
+
+
+class LogFileException(Exception):
+    """Custom exception."""
+
+    pass
+
+
+def get_log_file(proj_dir):
+    """Get last log file."""
+    spath = os.path.join(proj_dir, "run_history", "log",
+                         "*_Linkage Mapper.txt")
+    entries = sorted(glob.glob(spath), key=os.path.getctime, reverse=True)
+    last_lm_log = next(iter(entries or []), None)
+    if not last_lm_log:
+        raise LogFileException("Log file was not found.")
+    if FILE_PREFIX in last_lm_log:
+        raise LogFileException("Upgraded log file already exits.")
+    return last_lm_log
+
+
+def update_params(param_line):
+    """Update parameter list from  v1.x to v2.0."""
+    pline_heading = "Parameters:\t"
+
+    if param_line[0:12] != pline_heading:
+        raise LogFileException("Log file does not contain a Parameters line")
+
+    lm_arg = literal_eval(param_line[12:len(param_line) - 1])
+
+    lm_arg.append(lm_arg[17])  # MAXCOSTDIST (arg[18])
+    lm_arg.append(lm_arg[18])  # MAXEUCDIST (arg[19])
+    lm_arg.append('#')  # OUTPUTFORMODELBUILDER (arg[20])
+    lm_arg.append('#')  # LMCUSTSETTINGS (arg[21])
+
+    lm_arg[18] = lm_arg[16]  # BUFFERDIST
+    lm_arg[17] = 200000  # CWDTHRESH
+    lm_arg[16] = 'true'  # WRITETRUNCRASTER
+
+    return "{}{}".format(pline_heading, lm_arg)
+
+
+def main(proj_name=None):
+    """Upgrade v1.x logfile to v2.0 logfile."""
+    if proj_name is None:
+        proj_name = sys.argv[1]
+
+    try:
+        last_lm_log = get_log_file(proj_name)
+        new_log_file = last_lm_log.replace("_Linkage", FILE_PREFIX)
+
+        lines = open(last_lm_log).read().splitlines()
+        lines[4] = update_params(lines[4])
+        open(new_log_file, 'w').write('\n'.join(lines))
+    except LogFileException, err:
+        arcpy.AddError(err)
+    else:
+        arcpy.AddMessage(
+            "New Log file {} saved.".format(os.path.basename(new_log_file)))
+
+
+if __name__ == "__main__":
+    main()
